7fe93d5deb61be86a2f56717d0e659b8f99694fc
rachit74 <rachithooda09@gmail.com>
2024-09-27 21:18:57 +0530

    sprite display

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,13 @@ int main() {
     // Create a window with a title and resolution
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
 
+    // loading a spirte(object?) to dispay
+    sf::Texture texture;
+    if (!texture.loadFromFile("./assets/test.jpg")) {
+        return EXIT_FAILURE;
+    }
+    sf::Sprite sprite(texture);
+
     // Main loop: runs while the window is open
     while (window.isOpen()) {
         sf::Event event;
@@ -16,6 +23,9 @@ int main() {
 
         // Clear window
         window.clear();
+
+        // Draw sprite
+        window.draw(sprite);
 
         // Display content to the screen
         window.display();
